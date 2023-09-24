14a2d603cbf01de58a572b358dead68ada9089d3
khaoulafs <khaoulafsihi@gmail.com>
2023-09-24 15:04:40 -0700

    Tasks

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -6,29 +6,29 @@
  **/
 void shell_sort(int *array, size_t size)
 {
-	unsigned int gap = 1, i, j;
-	int temp;
-	if (array == NULL)
-		return;
-	if (size < 2)
-		return;
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-	while (gap > 0)
-	{
-		for (i = gap; i < size; i++)
-		{
-			temp = array[i];
-			j = i;
-			while (j >= gap && array[j - gap] > temp)
-			{
-				array[j] = array[j - gap];
-				j -= gap;
-			}
-			array[j] = temp;
-		}
-		print_array(array, size);
-		gap /= 3;
-	}
+unsigned int gap = 1, i, j;
+int temp;
+if (array == NULL)
+return;
+if (size < 2)
+return;
+while (gap < size / 3)
+gap = gap * 3 + 1;
+while (gap > 0)
+{
+for (i = gap; i < size; i++)
+{
+temp = array[i];
+j = i;
+while (j >= gap && array[j - gap] > temp)
+{
+array[j] = array[j - gap];
+j -= gap;
+}
+array[j] = temp;
+}
+print_array(array, size);
+gap /= 3;
+}
 }
 
--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -8,17 +8,17 @@
 */
 void stupify(int *array, int heap, int i, int size)
 {
-	int lar = i, left = 2 * i + 1, right = 2 * i + 2, t;
-	if (left < heap && array[left] > array[lar])
-		lar = left;
-	if (right < heap && array[right] > array[lar])
-		lar = right;
-	if (lar != i)
-	{
-		t = array[i], array[i] = array[lar], array[lar] = t;
-		print_array(array, size);
-		stupify(array, heap, lar, size);
-	}
+int lar = i, left = 2 * i + 1, right = 2 * i + 2, t;
+if (left < heap && array[left] > array[lar])
+lar = left;
+if (right < heap && array[right] > array[lar])
+lar = right;
+if (lar != i)
+{
+t = array[i], array[i] = array[lar], array[lar] = t;
+print_array(array, size);
+stupify(array, heap, lar, size);
+}
 }
 /**
 * heap_sort - Sorts array with heap sort algo
@@ -27,20 +27,19 @@ void stupify(int *array, int heap, int i, int size)
 */
 void heap_sort(int *array, size_t size)
 {
-	int i = size / 2 - 1, temp;
-	if (array == NULL || size < 2)
-		return;
-	for (; i >= 0; i--)
-		stupify(array, size, i, size);
-	for (i = size - 1; i >= 0; i--)
-	{
-		temp = array[0];
-		array[0] = array[i];
-		array[i] = temp;
-		if (i > 0)
-			print_array(array, size);
-		stupify(array, i, 0, size);
-	}
-
+int i = size / 2 - 1, temp;
+if (array == NULL || size < 2)
+return;
+for (; i >= 0; i--)
+stupify(array, size, i, size);
+for (i = size - 1; i >= 0; i--)
+{
+temp = array[0];
+array[0] = array[i];
+array[i] = temp;
+if (i > 0)
+print_array(array, size);
+stupify(array, i, 0, size);
+}
 }
 
